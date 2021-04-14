16cbee2731ee04944a332ed633617d1e6ea2aa08
qaz19990610 <qaz19990610@yahoo.com.tw>
2021-04-14 15:46:28 +0800

    0412

--- backup_.sh
+++ backup_.sh
@@ -3,12 +3,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 cd /git++
-read -p "commit what ?):" commit1
-git all .
+git add .
+read -p "commit what ? : " commit1
 git status
-git commit -m "$commit1"
+git commit -m  ${commit1}
 git log
-git checkout master
+q!
 git push origin master
 
 
