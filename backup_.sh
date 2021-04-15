e9be5d2d06eb41ea87bdd37627b6916be482fe14
qaz19990610 <qaz19990610@yahoo.com.tw>
2021-04-15 09:36:27 +0800

    0415backup

--- backup_.sh
+++ backup_.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+###04/14 ####
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 DATE=$(date +%m%d)
+BackUp=/home
+touch ${DATE}
 cd /git++
 git add .
-read -p "commit what ? : " commit1
+#read -p "commit what ? : " commit1
+
 git status
-git commit -m  ${commit1}
+#git commit -m  ${commit1}
+git commit -m " ${DATE}backup"
 #git log
 
 git push origin master
 cd /home
-tar -cvf backup${DATE}.tar /git++
+${BackUp}=tar -cvf backup${DATE}.tar /git++
 
